9b68603cbfbbae4726e7f734be1126a004e4b69b
Julian Hüppauff <julian@hueppauff.info>
2020-12-02 20:16:43 +0100

    added part 1, clean up

---
+++ Advent2/PasswordProperty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent2
+{
+    public class PasswordProperty
+    {
+        public int MinimumLength { get; set; }
+
+        public int MaximumLength { get; set; }
+
+        public char Character { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ Advent2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Advent2
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            string[] content = await File.ReadAllLinesAsync("data.txt").ConfigureAwait(false);
+            List<PasswordProperty> passwordProperties = new List<PasswordProperty>();
+
+            foreach (var item in content)
+            {
+                PasswordProperty passwordProperty = new PasswordProperty
+                {
+                    MinimumLength = Convert.ToInt32(item.Split('-')[0]),
+                    MaximumLength = Convert.ToInt32(((item.Split('-')[1]).Split(' ')[0])),
+                    Character = Convert.ToChar((item.Split(' ')[1]).Replace(":","")),
+                    Password = item.Split(' ')[2]
+                };
+
+                passwordProperties.Add(passwordProperty);
+            }
+
+            List<string> results = new List<string>();
+            int i = 0;
+
+            foreach (var passwordProperty in passwordProperties)
+            {
+                int countOfOccurence = passwordProperty.Password.Split(passwordProperty.Character).Length - 1;
+                if (countOfOccurence <= passwordProperty.MaximumLength && countOfOccurence >= passwordProperty.MinimumLength)
+                {
+                    Console.WriteLine($"Found : {passwordProperty.Password}");
+                    i++;
+                }
+            }
+
+            Console.WriteLine($"Found total amount of {i} Passwords");
+        }
+    }
+}
