00a36629e5fef3eb54b5cdfa8cd9d2f3559bded6
Julian Hüppauff <julian@hueppauff.info>
2020-12-03 18:41:29 +0100

    added 3 day

---
+++ Advent3/Position.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent3
+{
+    public class Position
+    {
+        /// <summary>
+        /// Int to the right
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Int to the bottom
+        /// </summary>
+        public int Y { get; set; }
+    }
+}
---
+++ Advent3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Advent3
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string[] content = await File.ReadAllLinesAsync("data.txt").ConfigureAwait(false);
+
+            char tree = '#';
+
+            Position position = new Position()
+            {
+                X = 0,
+                Y = 0
+            };
+
+            int counter = 0;
+
+            while (position.Y < content.Length - 1)
+            {
+                position = GetNewPosition(position);
+
+                if (content[position.Y][position.X] == tree)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine($"Found {counter} trees");
+        }
+
+        private static Position GetNewPosition (Position currentPosition)
+        {
+            currentPosition.X += 3;
+            currentPosition.Y += 1;
+
+            if (currentPosition.X >= 31)
+            {
+                currentPosition.X -= 31;
+            }
+
+            return currentPosition;
+        }
+    }
+}
