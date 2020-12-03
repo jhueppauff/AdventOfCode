0fe61059e618d422d23c8e9adfc19defb463ae2e
Julian Hüppauff <julian@hueppauff.info>
2020-12-03 19:01:05 +0100

    Day 3 part 2

--- Advent3/Program.cs
+++ Advent3/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Advent3
 {
     class Program
     {
+        private static List<Slope> slopes = new List<Slope>();
+
         static async Task Main(string[] args)
         {
             string[] content = await File.ReadAllLinesAsync("data.txt").ConfigureAwait(false);
@@ -17,26 +21,69 @@ namespace Advent3
                 X = 0,
                 Y = 0
             };
-
-            int counter = 0;
-
-            while (position.Y < content.Length - 1)
+            slopes.Add(new Slope()
             {
-                position = GetNewPosition(position);
+                X = 1,
+                Y = 1
+            });
+            slopes.Add(new Slope()
+            {
+                X = 3,
+                Y = 1
+            });
+            slopes.Add(new Slope()
+            {
+                X = 5,
+                Y = 1
+            });
+            slopes.Add(new Slope()
+            {
+                X = 7,
+                Y = 1
+            });
+            slopes.Add(new Slope()
+            {
+                X = 1,
+                Y = 2
+            });
 
-                if (content[position.Y][position.X] == tree)
+            List<int> listOfCounters = new List<int>();
+
+            foreach (var item in slopes)
+            {
+                int counter = 0;
+
+                while (position.Y < content.Length - 1)
                 {
-                    counter++;
+                    position = GetNewPosition(position, item);
+
+                    if (content[position.Y][position.X] == tree)
+                    {
+                        counter++;
+                    }
+                }
+
+                listOfCounters.Add(counter);
+                position.Y = 0; 
+                position.X = 0;
+            }
+
+            double result = 1;
+            foreach (var item in listOfCounters)
+            {
+                if (item != 0)
+                {
+                    result = result * item;
                 }
             }
 
-            Console.WriteLine($"Found {counter} trees");
+            Console.WriteLine($"Result: {result}");
         }
 
-        private static Position GetNewPosition (Position currentPosition)
+        private static Position GetNewPosition(Position currentPosition, Slope slope)
         {
-            currentPosition.X += 3;
-            currentPosition.Y += 1;
+            currentPosition.X += slope.X;
+            currentPosition.Y += slope.Y;
 
             if (currentPosition.X >= 31)
             {
---
+++ Advent3/Slope.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent3
+{
+    public class Slope
+    {
+        public int Y { get; set; }
+
+        public int X { get; set; }
+    }
+}
