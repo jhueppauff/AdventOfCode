edb68a13e9b89a6ec0f3576ef3d26af481a97ba3
Julian Hüppauff <julian@hueppauff.info>
2020-12-04 20:46:08 +0100

    Part 1

---
+++ Advent4/DictionaryExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent4
+{
+    public static class DictionaryExtension
+    {
+        public static Dictionary<TKey, TElement> ToSafeDictionary<TSource, TKey, TElement>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector, IEqualityComparer<TKey> comparer)
+        {
+            if (source == null)
+                throw new ArgumentException("source");
+            if (keySelector == null)
+                throw new ArgumentException("keySelector");
+            if (elementSelector == null)
+                throw new ArgumentException("elementSelector");
+            Dictionary<TKey, TElement> d = new Dictionary<TKey, TElement>(comparer);
+            foreach (TSource element in source)
+            {
+                if (!d.ContainsKey(keySelector(element)))
+                    d.Add(keySelector(element), elementSelector(element));
+            }
+            return d;
+        }
+    }
+}
---
+++ Advent4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Advent4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(File.ReadAllText("data.txt")
+                .Split("\r\n\r", StringSplitOptions.RemoveEmptyEntries)
+                .Where(o => o.Count(z => z == ':') == 8 || (o.Count(z => z == ':') == 7 && !o.Contains("cid")))
+                .Count());
+        }
+    }
+}
