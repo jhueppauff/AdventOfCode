702cb64f86ca6bd57b8a273f67267e12dc2ca011
Julian Hüppauff <julian@hueppauff.info>
2020-12-06 09:32:59 +0100

    Part 1

---
+++ Advent6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Advent6
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string content = await File.ReadAllTextAsync("data.txt").ConfigureAwait(false);
+
+            string[] groups = content.Split(new string[] { "\r\n\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            List<int> countOfAnsweres = new List<int>();
+
+            foreach (string group in groups)
+            {
+                countOfAnsweres.Add(group.Replace("\r\n", "").Distinct().Count());
+            }
+
+            int result = 0;
+
+            foreach (int count in countOfAnsweres)
+            {
+                result += count;
+            }
+
+            Console.WriteLine($"Result {result}");
+        }
+    }
+}
