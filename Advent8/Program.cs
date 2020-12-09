8938da91bb679f48ad04253595dba7ddb323d413
Julian Hüppauff <julian@hueppauff.info>
2020-12-09 21:11:24 +0100

    added next day

---
+++ Advent8/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Advent8
+{
+    class Program
+    {
+        static bool[] visited;
+        static bool isTerminated = false;
+
+        static async Task Main(string[] args)
+        {
+            string[] content = await File.ReadAllLinesAsync("data.txt").ConfigureAwait(false);
+            visited = new bool[content.Length];
+
+            Console.WriteLine("PartA: {0}", ExecuteProgram(content));
+            Console.WriteLine("PartB: {0}", PartB(content));
+        }
+
+        static int ExecuteProgram(string[] instructions)
+        {
+            int ip = 0;
+            int accumulator = 0;
+            string opCode = "";
+            string instruction = "";
+            bool[] visited = new bool[1000];
+
+            while (true)
+            {
+                if (ip == instructions.Length)
+                {
+                    isTerminated = true;
+                    return accumulator;
+                }
+
+                if (visited[ip] == true)
+                    return accumulator;
+
+                visited[ip] = true;
+
+                string tmp = instructions[ip].ToString();
+                string[] res = tmp.Split(null);
+                instruction = res[0].ToString();
+                opCode = res[1].ToString();
+
+                if (instruction == "nop")
+                {
+                    ip++;
+                }
+                else if (instruction == "acc")
+                {
+                    ip++;
+
+                    if (opCode[0].ToString() == "+")
+                    {
+                        accumulator = accumulator + int.Parse(opCode.Remove(0, 1).ToString());
+                    }
+                    else if (opCode[0].ToString() == "-")
+                    {
+                        accumulator = accumulator - int.Parse(opCode.Remove(0, 1).ToString());
+                    }
+                }
+                else if (instruction == "jmp")
+                {
+                    if (opCode[0].ToString() == "+")
+                    {
+                        ip = ip + int.Parse(opCode.Remove(0, 1).ToString());
+                    }
+                    else if (opCode[0].ToString() == "-")
+                    {
+                        ip = ip - int.Parse(opCode.Remove(0, 1).ToString());
+                    }
+                }
+            }
+            return 0;
+        }
+
+
+        static int PartB(string[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] cpyInput = new string[input.Length];
+                Array.Copy(input, cpyInput, input.Length);
+                int result = 0;
+
+                if (cpyInput[i].Substring(0, 3) == "nop")
+                {
+                    Array.Copy(input, cpyInput, input.Length);
+                    cpyInput[i] = cpyInput[i].Replace("nop", "jmp");
+                    result = ExecuteProgram(cpyInput);
+                }
+                if (cpyInput[i].Substring(0, 3) == "jmp")
+                {
+                    Array.Copy(input, cpyInput, input.Length);
+                    cpyInput[i] = cpyInput[i].Replace("jmp", "nop");
+                    result = ExecuteProgram(cpyInput);
+
+                }
+                if (isTerminated)
+                    return result;
+            }
+            return 0;
+
+        }
+    }
+}
---
+++ Advent9/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Advent9
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string[] content = await File.ReadAllLinesAsync("data.txt").ConfigureAwait(false);
+
+            Solve(content);
+        }
+
+        static void Solve(string[] input)
+        {
+            List<long> values = new List<long>();
+            foreach (string s in input)
+            {
+                values.Add(long.Parse(s));
+            }
+
+            long nextNumber = 0;
+            long part1 = 0;
+            int invalidIndex = 0;
+            List<long> sums = new List<long>();
+            for (int next = 25; next < values.Count; next++)
+            {
+                nextNumber = values[next];
+                sums.Clear();
+                for (int min = next - 25; min < next - 1; min++)
+                {
+                    for (int counter = 1; counter < 25; counter++)
+                    {
+                        sums.Add(values[min] + values[min + counter]);
+                    }
+                }
+                if (!sums.Contains(nextNumber))
+                {
+                    part1 = nextNumber;
+                    invalidIndex = values.FindIndex(x => x == nextNumber);
+                    break;
+                }
+            }
+
+            sums.Clear();
+            long part2 = 0;
+            for (int minRange = 0; minRange < invalidIndex - 2; minRange++)
+            {
+                sums.Add(values[minRange]);
+                for (int maxRange = minRange + 1; maxRange < invalidIndex; maxRange++)
+                {
+                    sums.Add(sums[^1] + values[maxRange]);
+                    if (sums[^1] == part1)
+                    {
+                        long low = long.MaxValue;
+                        long high = 0;
+                        for (int i = minRange; i <= maxRange; i++)
+                        {
+                            if (values[i] < low)
+                                low = values[i];
+                            else if (values[i] > high)
+                                high = values[i];
+                        }
+                        part2 = high + low;
+                    }
+                }
+            }
+
+            Console.WriteLine("Part 1: {0}", part1);
+            Console.WriteLine("Part 2: {0}", part2);
+        }
+    }
+}
